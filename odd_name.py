6c0233182ae4ea483d509f93c088fb24ef25d3c0
Dylan-Wharton <dylan.wharton@my.jcu.edu.au>
2018-03-16 09:44:48 +1000

    Added name ability to choose frequency of letter output

--- odd_name.py
+++ odd_name.py
@@ -1,4 +1,18 @@
-user_name = input("Please enter your name")
-while user_name == "":
-    user_name = input("Name cannot be blank, please enter your name")
-print (user_name[::2])
+def main():
+    user_name = get_name()
+    frequency = letter_frequency()
+    while user_name == "":
+        user_name = input("Name cannot be blank, please enter your name")
+    print (user_name[::frequency])
+
+
+def get_name():
+    user_name = input("Please enter your name")
+    return user_name
+
+def letter_frequency():
+    frequency = int(input("Enter an integer"))
+    return frequency
+
+
+main()
